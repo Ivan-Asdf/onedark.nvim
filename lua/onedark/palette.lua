bb68948b8a81a6692fada892638283db6ede1833
Ivan <ivan.asdf0@gmail.com>
2022-07-03 01:44:23 +0300

    Change bg color and nvim-tree bg color.

--- lua/onedark/palette.lua
+++ lua/onedark/palette.lua
@@ -169,11 +169,11 @@ return {
 	},
 	light = {
 		black = "#101012",
-		bg0 = "#fafafa",
+		bg0 = "#ffffff",
 		bg1 = "#f0f0f0",
 		bg2 = "#e6e6e6",
 		bg3 = "#dcdcdc",
-		bg_d = "#c9c9c9",
+		bg_d = "#f8fafc",
 		bg_blue = "#68aee8",
 		bg_yellow = "#e2c792",
 		fg = "#383a42",
